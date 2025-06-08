6bb5b2a1ed662bf3b337e8ffad97bfd128d0784b
Sujan TR <sujan.tr@carvewing.com>
2025-06-08 22:32:24 +0530

    Add project files.

---
+++ Section-2(Language Basics)/MyFirstApplication/FirstApp/FirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class Program // Every Application written should be wrapped within the class. The class name can be different from the file name.
+    {
+        static void Main(string[] args) // Entry point of the Application
+        {
+            /*
+                -> The name of the Main method must be capital 'M'.
+                -> The return type of the Main method must be always void(return nothing).
+                -> The Main method must be always static, because we can call that method without creating the object of the class.
+             */
+            System.Console.WriteLine("Hello this is my First Statement in C#");
+            System.Console.ReadKey(); // This line is written to keep the console window open
+        }
+    }
+}
---
+++ Section-2(Language Basics)/TestingSample/TestingSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSample
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            // Just created a new project having just boiler codes.
+        }
+    }
+}

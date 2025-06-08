93b41a0ce5e77e046ec1a4fe8a789fc235c41842
Sujan TR <sujan.tr@carvewing.com>
2025-06-08 22:47:33 +0530

    Explained regarding the Console Class

---
+++ Section-2(Language Basics)/ConsoleClass/AboutConsoleClass/AboutConsoleClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutConsoleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                -> The Console class in C# is part of the System namespace and provides methods for input and output operations in a console application.
+                -> It allows you to read from and write to the console window, making it essential for console-based applications.
+                -> It is a static class so we can use the methods of directly witout creating the object for the Console class. It is a part of BCL.
+                -> Members are:
+                    1. Write() - Writes the specified data to the console without a new line.
+                    2. WriteLine() - Writes the specified data to the console followed by a new line.
+                    3. ReadKey() - Reads a single key press from the console and returns it as a ConsoleKeyInfo object.
+                    4. Clear() - Clears the console window.
+                    5. ReadLine() - Reads a line of input from the console and returns it as a string. If we want numerical value we have to type cast.
+             */
+
+            // 1. Prints the message side-by-side
+            Console.Write("Welcome");
+            Console.Write("To");
+            Console.Write("C# Programming");
+
+            // 2. Prints the message line-by-line
+            Console.WriteLine("Welcome");
+            Console.WriteLine("To");
+            Console.WriteLine("C# Programming");
+
+            // 3. Read key to keep the console window open
+            Console.ReadKey();
+
+            // 4. Clear the screen
+            Console.Clear();
+            Console.WriteLine("Thank You");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section-2(Language Basics)/ConsoleClass/AboutConsoleClass/AboutConsoleClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AboutConsoleClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AboutConsoleClass")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0fb4515-1414-4a04-9196-e1723ee1481a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

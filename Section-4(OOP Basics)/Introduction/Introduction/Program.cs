90080ddeccc1a68ab7be171db8be6e8500d79a79
Sujan TR <sujan.tr@carvewing.com>
2025-06-12 04:57:06 +0530

    Added the basic explanation of classes and objects theory only

---
+++ Section-4(OOP Basics)/Introduction/Introduction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              WHAT IS OOP ?
+                -> Object-Oriented Programming (OOP) is a programming paradigm that one of the most popular for building the scalable applications.
+                -> OOP is used in most of the popular programming languages like C#, Java, Python, etc.
+                -> Goal of OOP is to group up some data and operations as a single unit called "Object".
+
+              WHAT IS OBJECT ?
+                -> Object is a small unit (entity) that represents a real-world person or thing.
+                -> Any physical object is called as Object
+                -> Object is a instace (example) of class.
+                -> Object stores the set of fields (details about the object)
+                -> All the objects are stored in the HEAL Memory
+
+              WHAT IS CLASS ?
+                -> Class is a model of object.
+                -> Class represents structure of data that you want to store in the similar objects. It is nothing but a blueprint of object.
+                -> Class is a collection of fields and methods that define the properties and behaviors of an object.
+                -> The class declares the fields that every object of that class will have and it will define the method that can be used by the object to perform some operations.
+                -> Default Access Modifier for all the classes are internal.
+            */
+        }
+    }
+}

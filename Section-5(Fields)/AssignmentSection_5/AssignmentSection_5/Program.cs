8b9bd8b19689771aab0610e4d49f561aba454157
Sujan TR <sujan.tr@carvewing.com>
2025-06-14 14:54:53 +0530

    Solved the Assignment of section 5

---
+++ Section-5(Fields)/AssignmentSection_5/AssignmentSection_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AssignmentSection_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.employeeId = 101;
+            employee1.employeeName = "John Doe";
+            employee1.employeeDesignation = "Software Engineer";
+            employee1.employeeSalary = 60000;
+
+            employee1.showEmployeeDetails();
+            Console.WriteLine();
+
+            Employee employee2 = new Employee();
+            employee2.employeeId = 102;
+            employee2.employeeName = "Jane Smith";
+            employee2.employeeDesignation = "Project Manager";
+            employee2.employeeSalary = 80000;
+
+            employee2.showEmployeeDetails();
+            Console.WriteLine();
+
+            Employee employee3 = new Employee();
+            employee3.employeeId = 103;
+            employee3.employeeName = "Alice Johnson";
+            employee3.employeeDesignation = "Data Analyst";
+            employee3.employeeSalary = 70000;
+
+            employee3.showEmployeeDetails();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section-5(Fields)/AssignmentSection_5/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Employee
+{
+    public int employeeId;
+    public string employeeName;
+    public string employeeDesignation;
+    public double employeeSalary;
+
+    public void showEmployeeDetails()
+    {
+        Console.WriteLine("Employee ID: " + employeeId);
+        Console.WriteLine("Employee Name: " + employeeName);
+        Console.WriteLine("Employee Designation: " + employeeDesignation);
+        Console.WriteLine("Employee Salary: " + employeeSalary);
+    }
+
+}

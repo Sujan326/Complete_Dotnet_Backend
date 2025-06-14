49373e1b9a16ebd575483859399ba3663e298375
Sujan TR <sujan.tr@carvewing.com>
2025-06-14 16:17:41 +0530

    Explained the concept of Understading the methods

---
+++ Section-6(Methods)/UnderstandingMethods/UnderstandingMethods/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace UnderstandingMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+                -> Methods are group of statements that perform specific task based on the fields.
+                -> Methods are the part of class.
+                -> Methods can receive one more parameters as input and can return a value.
+                -> In methods the parameters are optional.
+                -> In methods accessModifiers, modifier are also optional.
+                -> In order to execute the Method we should call the method after declaring it.
+                -> Default access modifiers for methods is Private.
+                -> Syntax:
+                    accessModifier modifier returnType methodName(parameter1, parameter2,...) 
+                    {
+                        // method body
+                    }
+                -> Method Call:
+                    methodName();
+            */
+
+            //local constant
+            const string developerName = "Sujan";
+
+            //create reference variables
+            Product product1, product2, product3;
+
+            //create objects
+            product1 = new Product();
+            Product.TotalNoProducts++; //1
+            product2 = new Product();
+            Product.TotalNoProducts++; //2
+            product3 = new Product();
+            Product.TotalNoProducts++; //3
+
+            //initalize fields
+            product1.productID = 1001;
+            product1.productName = "Mobile";
+            product1.cost = 20000;
+            product1.quantityInStock = 1200;
+
+            product2.productID = 1003;
+            product2.productName = "Laptop";
+            product2.cost = 45000;
+            product2.quantityInStock = 3400;
+
+            product3.productID = 1003;
+            product3.productName = "Speakers";
+            product3.cost = 36000;
+            product3.quantityInStock = 800;
+
+            // Tax Calculation Method Call
+            product1.CalculateTax();
+            product2.CalculateTax();
+            product3.CalculateTax();
+
+            //get values from fields
+            Console.WriteLine(developerName);
+
+            Console.WriteLine("Product 1:");
+            Console.WriteLine("Product ID: " + product1.productID);
+            Console.WriteLine("Product Name: " + product1.productName);
+            Console.WriteLine("Cost: " + product1.cost);
+            Console.WriteLine("Quantity in Stock: " + product1.quantityInStock);
+            Console.WriteLine("Date of Purchase: " + product1.dateOfPurchase);
+            Console.WriteLine("Tax: " + product1.tax);
+
+            Console.WriteLine("\nProduct 2:");
+            Console.WriteLine("Product ID: " + product2.productID);
+            Console.WriteLine("Product Name: " + product2.productName);
+            Console.WriteLine("Cost: " + product2.cost);
+            Console.WriteLine("Quantity in Stock: " + product2.quantityInStock);
+            Console.WriteLine("Date of Purchase: " + product2.dateOfPurchase);
+            Console.WriteLine("Tax: " + product2.tax);
+
+            Console.WriteLine("\nProduct 3:");
+            Console.WriteLine("Product ID: " + product3.productID);
+            Console.WriteLine("Product Name: " + product3.productName);
+            Console.WriteLine("Cost: " + product3.cost);
+            Console.WriteLine("Quantity in Stock: " + product3.quantityInStock);
+            Console.WriteLine("Date of Purchase: " + product3.dateOfPurchase);
+            Console.WriteLine("Tax: " + product3.tax);
+
+            int totalQuantity = product1.quantityInStock + product2.quantityInStock + product3.quantityInStock;
+            Console.WriteLine("Total Quantity: " + totalQuantity);
+            Console.WriteLine("Total no. of products: " + Product.TotalNoProducts);  //Output: 3
+            Console.WriteLine("Category of products: " + Product.CategoryName);  //Output: Electronics
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section-6(Methods)/UnderstandingMethods/UnderstandingMethods/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnderstandingMethods")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnderstandingMethods")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51791429-3240-4257-97d8-1cab4a0c7ba0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

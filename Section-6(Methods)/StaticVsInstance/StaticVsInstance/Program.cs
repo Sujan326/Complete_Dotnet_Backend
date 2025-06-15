1399d2ceb15095728647bd5a19ad7a0171f1feed
Sujan TR <sujan.tr@carvewing.com>
2025-06-15 13:03:10 +0530

    Explained and added the Static vs Instance concept

---
+++ Section-6(Methods)/StaticVsInstance/StaticVsInstance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVsInstance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example obj = new Example();
+            obj.InstanceMethod(); // Calling instance method
+            Console.WriteLine();
+            Example.StaticMethod(); // Calling static method
+        }
+    }
+
+    class Example
+    {
+        public static string staticVariable = "I am static variable";
+        public string instanceVariable = "I am instance variable";
+
+        public void InstanceMethod()
+        {
+            Console.WriteLine("Inside Instance Method");
+            Console.WriteLine(instanceVariable);
+            Console.WriteLine(staticVariable);
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Inside Static Method");
+            Console.WriteLine(staticVariable);
+            // Console.WriteLine(instanceVariable); // This line will cause a compile-time error
+
+            Example ob = new Example();
+            Console.WriteLine(ob.instanceVariable); // This is valid.
+        }
+    }
+}

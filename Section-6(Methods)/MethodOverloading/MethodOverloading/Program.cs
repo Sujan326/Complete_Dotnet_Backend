97696c0fa87da10a04d9408d25dabc6df4e95117
Sujan TR <sujan.tr@carvewing.com>
2025-06-15 15:23:40 +0530

    Explained the concept of Method Overloading

---
+++ Section-6(Methods)/MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Example
+    {
+        public int AddNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public double AddNumbers(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                -> Writing multiple methods with the same name but with different parameters in the same class is called as Method Overloading.
+                -> Caller will be having several options to use the same method with different types of parameters.
+            */
+
+            Example example = new Example();
+            Console.WriteLine(example.AddNumbers(1, 2)); // Here the user will be having multiple option so they can pass either the decimal value or the whole integers.
+        }
+    }
+}

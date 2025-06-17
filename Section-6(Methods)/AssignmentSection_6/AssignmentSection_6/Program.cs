19da7bf3c80255d141e461017f97464d6cc04ecd
Sujan TR <sujan.tr@carvewing.com>
2025-06-17 06:28:23 +0530

    Solved the assignment question of section-6, regaring creating a medical bot

---
+++ Section-6(Methods)/AssignmentSection_6/AssignmentSection_6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentSection_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hi, I'm {MedicalBot.GetBotName()}. I'm here to help you in your medication.");
+
+            Patient patient = new Patient();
+
+            Console.WriteLine("Enter your (patient) details: ");
+
+            Console.Write("Enter Patient Name: ");
+            patient.SetName(Console.ReadLine(), out string nameError);
+
+            Console.Write("\nEnter Patient Age: ");
+            patient.SetAge(int.Parse(Console.ReadLine()), out int ageError);
+
+            Console.Write("\nEnter Patient Gender: ");
+            patient.SetGender(Console.ReadLine(), out string genderError);
+
+            Console.Write("\nEnter Medical History (Eg: Diabetes). Press Enter for None: ");
+            patient.SetMedicalHistory(Console.ReadLine(), out string medicalHistoryError);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Welcome {patient.GetName()}, {patient.GetAge()} ");
+            Console.WriteLine("Which of the following symptom do you have ?");
+            Console.WriteLine("S1. Headache\nS2. Skin rashes\nS3. Dizziness");
+            Console.WriteLine("Enter the symptom code from above list (S1, S2 or S3): ");
+            patient.SetSymptomeCode(Console.ReadLine(), out string symptomeCodeError);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Your prescription based on your age, symptoms and medical history:");
+
+            MedicalBot medicalBot = new MedicalBot();
+            medicalBot.PrescribeMedication(patient);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Section-6(Methods)/AssignmentSection_6/MedicalBot/Class1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+public class MedicalBot
+{
+    private const string botName = "Bob";
+
+    public static string GetBotName()
+    {
+        return botName;
+    }
+
+    public void PrescribeMedication(Patient patient)
+    {
+        string dosage = GetDosage(patient.GetSymptomeCode());
+        if (patient.GetSymptomeCode().Contains("s1")) // HeadAche
+        {
+            Console.WriteLine("Medicine: Ibuprofen.");
+            Console.WriteLine($"Dosage: {dosage}");
+        }
+        else if (patient.GetSymptomeCode().Contains("s2")) // Skin Rashes
+        {
+            Console.WriteLine("Medicine: Diphenhydramine.");
+            Console.WriteLine($"Dosage: {dosage}");
+        }
+        else if (patient.GetSymptomeCode().Contains("s3")) // Dizziness
+        {
+            if (patient.GetMedicalHistory().Contains("Diabetes"))
+            {
+                Console.WriteLine("Medicine: Metformin.");
+                Console.WriteLine($"Dosage: 500 mg");
+            }
+            else
+            {
+                Console.WriteLine("Medicine: Dimenhydrinate.");
+                Console.WriteLine($"Dosage: {dosage}");
+            }
+        }
+
+        string GetDosage(string medicineName)
+        {
+            switch (patient.GetSymptomeCode())
+            {
+                case "s1": // ibuprofen
+                    if (patient.GetAge() < 18)
+                    {
+                        return "400 mg";
+                    }
+                    else
+                    {
+                        return "800 mg";
+                    }
+                case "s2": // diphenhydramine
+                    if (patient.GetAge() < 18)
+                    {
+                        return "50 mg";
+                    }
+                    else
+                    {
+                        return "300 mg";
+                    }
+                case "s3": // dimenhydrinate
+                    if (patient.GetAge() < 18)
+                    {
+                        return "50 mg";
+                    }
+                    else
+                    {
+                        return "400 mg";
+                    }
+                default:
+                    return "Dosage not available for this medication.";
+            }
+        }
+    }
+}
+
+public class Patient
+{
+    private string name;
+    private int age;
+    private string gender;
+    private string medicalHistory;
+    private string symptomeCode;
+    private string prescription;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetName(string value, out string errorMessage)
+    {
+        if (value != null && value.Length > 0)
+        {
+            name = value;
+            errorMessage = "";
+        }
+        else
+        {
+            errorMessage = "Name cannot be null or empty.";
+        }
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public void SetAge(int value, out int errorMessage)
+    {
+        if (value > 0)
+        {
+            age = value;
+            errorMessage = 0; // No Error, Positive Number Entered
+        }
+        else
+        {
+            errorMessage = 1; // Negative Number Entered
+        }
+    }
+
+    public string GetGender()
+    {
+        return gender;
+    }
+
+    public void SetGender(string value, out string errorMessage)
+    {
+        if (value != null && value.Length > 0)
+        {
+            gender = value;
+            errorMessage = "";
+        }
+        else
+        {
+            errorMessage = "Gender field cannot be null or empty.";
+        }
+    }
+
+    public string GetMedicalHistory()
+    {
+        return medicalHistory;
+    }
+
+    public void SetMedicalHistory(string value, out string errorMessage)
+    {
+        if (value != null || value.Length > 0)
+        {
+            medicalHistory = value;
+            errorMessage = "";
+        }
+        else
+        {
+            errorMessage = "No medical history";
+        }
+    }
+
+    public string GetSymptomeCode()
+    {
+        return symptomeCode;
+    }
+
+    public void SetSymptomeCode(string value, out string errorMessage)
+    {
+        if (value != null && value.Length > 0)
+        {
+            symptomeCode = value;
+            errorMessage = "";
+        }
+        else
+        {
+            errorMessage = "Symptome code cannot be null or empty.";
+        }
+    }
+
+    public string GetPrescription()
+    {
+        return prescription;
+    }
+
+    public void SetPrescription(string value, out string errorMessage)
+    {
+        if (value != null && value.Length > 0)
+        {
+            prescription = value;
+            errorMessage = "";
+        }
+        else
+        {
+            errorMessage = "Prescription cannot be null or empty.";
+        }
+    }
+
+}
